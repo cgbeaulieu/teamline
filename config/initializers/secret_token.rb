c38afa4b8201eead76e30d93594266c74198e640
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-06 16:25:37 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamline::Application.config.secret_token = '9d1e069f3798902aae0d44508df880593c42ac212bd1f7c11c95f8ec97dbad797ca1d19776069f7f471272d9fd08b095548d1efc08b725980b5e51595644c825'
