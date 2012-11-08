e8af86a95ac0720a7431144779768d11c35c0af1
Matthew Salerno <matthewsalern@gmail.com>
2012-11-08 17:22:25 -0500

    commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :url
+  attr_accessible :content, :title, :url, :created_at
 end
--- app/models/rss_parser.rb
+++ app/models/rss_parser.rb
@@ -2,7 +2,7 @@ class RssParser
 
   def self.posts_from_feed(feedurl)
     feed = Feedzirra::Feed.fetch_and_parse(feedurl)
-    feed.entries.each{|entry| Post.create(:title => entry.title, :content => entry.content, :url => entry.url, :created_at => entry.published)}
+    feed.entries.each{|entry| Post.create(:title => entry.title, :content => entry.summary, :url => entry.url, :created_at => entry.published)}
   end
 
 end
