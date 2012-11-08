da0bdedd90f988905fc3affd1cdf1de9eaf9a920
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-08 17:11:26 -0500

    refactored creating post from feed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title, :url, :created_at
   default_scope order('created_at DESC')
+
+  def self.create_from_feed(feed)
+    feed.entries.each do |entry|
+      create(:title => entry.title, :content => entry.content, 
+             :url => entry.url, :created_at => entry.published)    
+    end
+  end
 end
--- app/models/rss_parser.rb
+++ app/models/rss_parser.rb
@@ -1,11 +1,8 @@
 class RssParser
-  def self.posts_from_feed(feed)
-    feed.entries.each{|entry| Post.create(:title => entry.title, :content => entry.content, :url => entry.url, :created_at => entry.published)}
-  end
 
   def self.fetch_feeds(*feed_urls)
     feeds = Feedzirra::Feed.fetch_and_parse(feed_urls)
-    feeds.each_value { |value| self.posts_from_feed(value) }
+    feeds.each_value { |feed| Post.create_from_feed(feed) }
   end
 
   def self.update_feeds(feeds)
