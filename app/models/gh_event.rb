37d24565f15952c6f1a37490ecaebb5f703aa029
Ericka Ward <ericka.ward@gmail.com>
2012-11-08 20:44:48 -0500

    refactor Octokit Wrapper and added username to GhEvents Table

--- app/models/gh_event.rb
+++ app/models/gh_event.rb
@@ -1,3 +1,8 @@
 class GhEvent < ActiveRecord::Base
-  attr_accessible :eventdate, :category
+  attr_accessible :eventdate, :category, :username
+
+  def self.create_from_octokit_event(event)
+    self.create(:eventdate => event.created_at, :category => event.type, :username => event.actor.login)
+  end
+
 end
--- app/models/octokit_wrapper.rb
+++ app/models/octokit_wrapper.rb
@@ -1,8 +1,16 @@
 class OctokitWrapper
   # attr_accessible :title, :body
 
-  def get_gh_events(ghuser)
-  	Octokit.user_events(ghuser).each {|event| GhEvent.create(:eventdate => event.created_at, :category => event.type) }  		  
+  def self.get_gh_events(ghuser)
+  	Octokit.user_events(ghuser).each {|event| GhEvent.create_from_octokit_event(event) }  		  
   end
 
+  # def get_gh_events(ghuser_array)
+  #   ghuser_array.map do |ghuser|
+  #     Octokit.user_events(ghuser)
+  #     #some type of update method
+  #   end
+  # end
+
 end
+
