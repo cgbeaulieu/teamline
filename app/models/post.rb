6b71d3b5c418f96cf8d4f8270af4b17b456da23f
Ericka Ward <ericka.ward@gmail.com>
2012-11-12 16:24:51 -0500

    refactored post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,24 +6,15 @@ class Post < ActiveRecord::Base
   default_scope order('published_at DESC')
 
   def self.update_from_feed(feed)
+    feed_url = feed.feed_url
     feed.entries.each do |entry|
-      create_from_entry(entry) unless Post.find_by_url(entry.url)
+      create_from_entry(entry, feed_url) unless Post.find_by_url(entry.url)
     end
   end
 
-  def self.create_from_entry(entry)
+  def self.create_from_entry(entry, feed_url)
     create(:title => entry.title, :content => entry.content, 
-           :url => entry.url, :published_at => entry.published, :person => Person.find_by_rss_feed(feed_url_from_entry_url(entry.url)))    
-  end
-
-private
-  def self.domain_from_entry_url(entry_url)
-    URI.parse(entry_url).hostname
-  end
-
-  def self.feed_url_from_entry_url(entry_url)
-    "http://#{domain_from_entry_url(entry_url)}/atom.xml"
+           :url => entry.url, :published_at => entry.published, :person => Person.find_by_rss_feed(feed_url))    
   end
 end
-
 
