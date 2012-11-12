fc01235f425490983156d98402dbe5351455ceda
Ericka Ward <ericka.ward@gmail.com>
2012-11-12 12:20:54 -0500

    building domain_from_entry_url method

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,11 @@ class Post < ActiveRecord::Base
 
   def self.create_from_entry(entry)
     create(:title => entry.title, :content => entry.content, 
-           :url => entry.url, :published_at => entry.published)    
+           :url => entry.url, :published_at => entry.published, :person_id => Person.find_by_rss_feed(rss_fee))    
+  end
+
+  def domain_from_entry_url(entry_url)
+    URI.parse(entry_url).hostname
   end
 end
 
