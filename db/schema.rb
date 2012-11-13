015154380afe6aebbf3429ededb178b8d16406b2
David Baker <2012@davidbaker.tv>
2012-11-13 12:12:22 -0500

    fixed kevins rookie mistake

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,20 @@
 
 ActiveRecord::Schema.define(:version => 20121113164905) do
 
+  create_table "commit_parsers", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "commits", :force => true do |t|
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "username"
+    t.string   "content"
+    t.string   "commit_time"
+    t.string   "link"
+  end
+
   create_table "gh_events", :force => true do |t|
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
@@ -20,8 +34,8 @@ ActiveRecord::Schema.define(:version => 20121113164905) do
     t.string   "content"
     t.date     "published_at", :limit => 255
     t.string   "username"
-    t.string   "url"
     t.integer  "person_id"
+    t.string   "url"
   end
 
   create_table "octokit_wrappers", :force => true do |t|
