cd57771650c7fe260ad3da1ff994751faad738eb
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-17 21:16:40 -0500

    extracted some jscript stuff

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,36 +1,60 @@
 $(function(){
   var last_date = $('li:first').data('date');
-  setTimeout(pollServer(last_date), 2000)
+  setTimeout(pollServer(last_date), 2000);
 })
 
 function pollServer(last_date){
   setTimeout(function(){
     $.get(
       '/timeline/poll', 
-      { created_at: last_date},
+      { created_at: last_date },
       function(json){
-        var last_event;
-        var date_query;
-        console.log(json.length);
-        
-        $.each(json, function(i, object){
-          last_event = object;
-          var time_ago = last_event.published_at;
-          $("ol:first").prepend("<li data-date=" + object.created_at + ">" + "<span>" + time_ago + "</span><p>" + 
-                                 object.content + "</p></li>");
-        });
-
-        if(last_event == null){
-          date_query = last_date
-        } else {
-          date_query = last_event.created_at.replace(/Z/,'').replace(/T/, ' ');
-        }
-
-        console.log(date_query);
+        var last_event = json[json.length-1];        
+        var date_query = getQueryDate(last_event, last_date);
+        displayNew(json);
         pollServer(date_query);
       }, 
     'json')
   }, 3000);
 };
+
+function formatTimestamp(timestamp){
+  return timestamp.replace(/Z/,'').replace(/T/, ' ');
+}
+
+function getQueryDate(last_event, last_date) {
+  if(last_event == null){
+    return last_date
+  } else {
+    return formatTimestamp(last_event.created_at);
+  }
+}
+
+function displayNew(collection){
+  $.each(collection, function(i, object){
+    insertEvent(object);
+  });
+}
+
+function insertEvent(object){
+  $("ol:first").prepend("<li class='right' data-date=" + 
+    object.created_at + "><i class='pointer'></i><div class='unit-tweet'><div class='storyUnit'><p>" + 
+    object.content + "</p></div><ol class='storyActions'><li>" + 
+    formatTimestamp(object.published_at) + "</li></ol></div></li>");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
