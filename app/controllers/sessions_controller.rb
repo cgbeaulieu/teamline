fc07519ac82768a10da1b5390b4c58e929fdb78a
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-26 12:50:44 -0500

    added signup redirect to edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     person = Person.from_omniauth(env["omniauth.auth"])
     session[:person_id] = person.id
-    redirect_to root_url, notice: "Signed in!"
+    redirect_to edit_person_path(person.id), notice: "Signed in!"
   end
 
   def destroy
