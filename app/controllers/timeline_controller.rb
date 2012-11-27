eec03f0481c43d524f27666c91998c6d3722c23d
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-27 15:31:00 -0500

    added timeline and controller changes on master

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,18 +1,25 @@
 class TimelineController < ApplicationController
-	def index
-    all_events = [GhEvent.all, Post.all, Tweet.all].flatten
-    sorted_events = Sorter.sort_and_group_by_published_at(all_events)
-    @events = Sorter.sorted_hash_to_array(sorted_events).paginate(:page => params[:page], :per_page => 5)
+  def index
+    timeline = Timeline.new
+    timeline.load_recent_events(20)
+    grouped_events = timeline.group_by_date
+    @grouped_events = grouped_events.paginate(:page => params[:page], :per_page => 5)
   end
 
   def poll
     if params[:published_at]
-      new_events = Poller.find_new_events(params[:published_at])
-      @events = Sorter.sort_by_published_at_asc(new_events)
-      render :json => @events
+      timeline = Timeline.new
+      timeline.find_new_events(params[:published_at])
+      @sorted_events = timeline.sort_events_ascending
+      #TODO: Render HTML instead of JSON
+      render :json => @sorted_events
       # render :json => render_to_string(@events)
     else
       render :json => 'empty'
     end
+  end
+
+  def filter
+    #params => some 
   end
 end
---
+++ app/models/timeline.rb
@@ -0,0 +1,44 @@
+class Timeline
+  Events = ['GhEvent', 'Post', 'Tweet']
+
+  attr_accessor :events
+
+  def load_recent_events(limit)
+    class_limit = limit/(Events.length)
+    @events = Events.map { |class_name| class_name.constantize.order('published_at DESC').
+      limit(class_limit) }.flatten
+  end
+
+  def group_by_date
+    events.group_by { |event| format_published_at(event) }.
+    map { |date, events| [date, events] }
+  end
+
+  def find_new_events(date)
+    query_date = parse_date(date)
+    @events = Events.map { |class_name| class_name.constantize.
+      where('published_at > ?', query_date) }.flatten
+  end
+
+  def sort_events_ascending
+    events.sort_by { |event| event.published_at }
+  end
+
+  def filter(type, params)
+    #delegates to the correct filter action
+  end
+
+  def filter_by_date
+    #...
+  end
+
+  private
+  
+    def format_published_at(event)
+      event.published_at.to_s(:db).split(" ").first 
+    end
+
+    def parse_date(date)
+      DateTime.parse(published_at) + 1.seconds
+    end
+end
