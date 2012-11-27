19773ff7259c812a3e0cfc9d3979c1915158c7ff
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-27 15:55:08 -0500

    changed method order in octokit

--- app/models/octokit_wrapper.rb
+++ app/models/octokit_wrapper.rb
@@ -3,12 +3,6 @@ class OctokitWrapper
     Octokit.user_events(ghuser).each { |event| self.parse_content(event) }
   end
 
-  # TODO: There is a way to refactor this using subclasses
-  # it's hard and not important but in theory
-  # if there was a class defined PushEvent < OctoKitEvents
-  # then you just constantize (turn the string 'PushEvent')
-  # into the actual ruby class and then call a method on it
-  # so each subclass defines it's own parser
   def self.parse_content(event)
     # if GithubEvent::VALID_EVENTS.include?(event.type)
     begin 
@@ -17,7 +11,7 @@ class OctokitWrapper
     rescue NameError
     end
   end
-  
+
   def self.create_gh_event(event)
     GhEvent.create_from_octokit_event(event)
   end
