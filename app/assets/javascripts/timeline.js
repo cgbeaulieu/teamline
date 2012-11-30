71a162259da1c5af68f0147a41f79fb2756d1bd4
Matthew Salerno <matthewsalern@gmail.com>
2012-11-30 11:30:47 -0500

    slideToggle js now bound to body and working with infinite scroll

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,23 +1,109 @@
+$(document).ready(function(){
+
+  $("#teamline").on("mouseenter", ".event", function(){
+    $(".secondary", this).slideToggle(500);
+  })
+
+  $("#teamline").on("mouseleave", ".event", function(){
+    $(".secondary", this).slideToggle(500);
+  })
+
+  var filterToggle = false;
+  
+  $('.datepicker').pickadate({
+      format_submit: 'yyyy-mm-dd',
+      onSelect: function() {
+        filterToggle = true;
+        var date = $("input[type='hidden']").val();
+        $.get('/timeline/filter', { date: date }, 
+            function(data){
+              $("ol.timeline").html(data);
+          });
+      }
+  });
+
+  $('#resourcefilter').submit(function(e){
+    e.preventDefault();
+
+    filterToggle = true;
+    var types = []
+    $.each($("input:checked"), function(){
+      var type = $(this).attr("id");
+      types.push(type);
+    });
+
+    $.get('/timeline/filter', { types: types }, function(data) {
+      $("ol.timeline").html(data);
+    });
+  });
+
+  $('#peoplefilter').submit(function(e){
+    e.preventDefault();
+
+    var people = []
+    $.each($("input:checked"), function(){
+      var person = $(this).attr("id");
+      people.push(person);
+    });
+
+    $.get('/timeline/filter', { people: people }, function(data) {
+      console.log(data)
+      $("ol.timeline").html(data);
+    });
+
+  });
+
+  var loadScrollDetect = function(){
+    if ($(window).scrollTop() > $(document).height() - $(window).height() - 200) {    
+      $(window).unbind('scroll');
+      loadEvents();
+    }
+  };
+
+  var loadEvents = function(){
+    var last_date = $('ol.timeline li.item:last').data('date');
+    $.ajax({
+      url: 'timeline/infinite',
+      data: {published_at: last_date},
+      success: function(data){
+        if (data.length > 1){
+          $("ol.timeline").append(data);
+        } else {
+          var new_date = moment(last_date).subtract('days', 1).format('YYYY-MM-DD');
+          new_date = String(new_date);
+          $("ol.timeline li.item:last").after("<li class='item' display='none;' data-date='" + new_date + "'></li>");
+        }
+      },
+      complete: function(date, textStatus){
+        $(window).bind('scroll', loadScrollDetect);
+      }
+    });
+  }
 
 
-// $(setInterval(function(){
-//   if(filterToggle === false){
-//     var raw_date  = $('ol.timeline li:first').data('date');
-//     var last_date = formatTimestamp(raw_date);
-//     console.log(last_date);
-//     $.ajax({
-//       url: '/timeline/poll',
-//       data: {published_at: last_date},
-//       success: function(data){
-//         $("div#date-header:first").after(data);
-//       },
-//       error: function(data){
-//         console.log(data);
-//       }
-//     });
-//   }
-// }, 3000));
 
-// function formatTimestamp(timestamp){
-//   return timestamp.replace(/Z/,'').replace(/T/, ' ');
-// }
+  $(window).bind('scroll', loadScrollDetect);
+
+  $(setInterval(function(){
+    if(filterToggle === false){
+      var raw_date  = $('ol.timeline li.item:first').data('date');
+      var last_date = formatTimestamp(raw_date);
+      console.log(last_date);
+      $.ajax({
+        url: '/timeline/poll',
+        data: {published_at: last_date},
+        success: function(data){
+          $("div#date-header:first").after(data);
+        },
+        error: function(data){
+          console.log(data);
+        }
+      });
+    }
+  }, 3000));
+
+function formatTimestamp(timestamp){
+  return timestamp.replace(/Z/,'').replace(/T/, ' ');
+}
+
+});
