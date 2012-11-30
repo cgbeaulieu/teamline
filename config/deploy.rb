1bfffeb68ab4948fe45f5048ad2b7e072e40ac63
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-30 16:32:05 -0500

    Symlink db

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ role :db,  "96.8.123.76", :primary => true # This is where Rails migrations will
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+before "deploy:assets:precompile", "deploy:symlink_database"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -31,5 +32,9 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :symlink_database, :roles => :app do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config" 
   end
 end
