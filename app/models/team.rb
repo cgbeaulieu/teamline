5487dc55d74e7095327ac35c74bda2f39809031c
Kevin Curtin <kevincurtin88@gmail.com>
2012-12-11 12:23:51 -0500

    commiting

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
   attr_accessible :name, :website, :subdomain, :people_attributes
   has_many :people
-  accepts_nested_attributes_for :people
+  accepts_nested_attributes_for :people, allow_destroy: true
 end
