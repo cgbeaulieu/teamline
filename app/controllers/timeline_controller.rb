795635a56a1a2c58bfe0e509aef4bac7ba6652c1
Kevin Curtin <kevincurtin88@gmail.com>
2012-12-17 10:05:12 -0500

    test

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,24 +2,19 @@ class TimelineController < ApplicationController
   before_filter :current_team
 
   def index
-    timeline = Timeline.new
-<<<<<<< HEAD
-    timeline.load_recent_events(3, @current_team)
-=======
-    timeline.load_recent_events(2, @current_team.id)
->>>>>>> started refacts
+    timeline = Timeline.new(2.days.ago.to_date.to_s)
+    timeline.load_recent_events(@current_team.id)
     timeline.group_by_date
-    # debugger
     @grouped_events = timeline.events
   end
 
   def poll
     if params[:published_at]
-      timeline = Timeline.new
-      timeline.find_new_events(params[:published_at], @current_team)
+      date = params[:published_at]
+      timeline = Timeline.new(DateTime.parse(date) + 1.seconds)
+      timeline.find_new_events(@current_team.id)
       timeline.sort_events_descending
-      @grouped_events = timeline.events
-      @json = render_to_string :partial => "poll_event", :locals => {:grouped_events => @grouped_events}
+      @json = render_to_string :partial => "poll_event", :locals => {:grouped_events => timeline.events}
       render :json => @json.to_json
     else
       render :json => 'empty'
@@ -28,12 +23,12 @@ class TimelineController < ApplicationController
 
   def infinite
     if params[:published_at]
-      timeline = Timeline.new
-      timeline.get_next_day(params)
+      date = DateTime.parse(params[:published_at]).yesterday
+      timeline = Timeline.new(date.at_beginning_of_day, date.end_of_day)
+      timeline.query(params)
       timeline.group_by_date
-      @grouped_events = timeline.events
-      @json = render_to_string :partial => "event", :locals => {:grouped_events => @grouped_events}
-      render :json => @json.to_json      
+      @json = render_to_string :partial => "event", :locals => {:grouped_events => timeline.events}
+      render :json => @json.to_json
     else
       render :json => 'empty'
     end
@@ -41,14 +36,20 @@ class TimelineController < ApplicationController
 
   def filter
     if params[:filter] || params.key?('start_date')
-      timeline = Timeline.new
-      timeline.filter(params)
+      start_date = params.fetch('start_date', nil)
+      if start_date
+        start_date = start_date.to_date.at_beginning_of_day
+        end_date   = start_date.end_of_day
+        timeline = Timeline.new(start_date, end_date)
+      else
+        timeline = Timeline.new(2.days.ago.to_date.to_s)
+      end
+
+      timeline.query(params.fetch(:filter, nil))
       timeline.group_by_date
-      @grouped_events = timeline.events
-      @json = render_to_string :partial => "event", :locals => {:grouped_events => @grouped_events}
+      @json = render_to_string :partial => "event", :locals => {:grouped_events => timeline.events}
       render :json => @json.to_json
     end
   end
-  
 end
 
--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,72 +1,32 @@
+DateRange = Struct.new(:start_date, :end_date)
+
 class Timeline
   Events = ['GhEvent', 'Post', 'Tweet']
 
-  attr_accessor :events
+  attr_accessor :events, :date_range
 
-  def load_recent_events(from, team_id)
-    query_date = from.days.ago.to_date.to_s
-    unsorted_events = query_for_date(query_date, team_id)
-    @events = unsorted_events.sort_by { |event| event.published_at }.reverse
-  end
-  
-  def find_new_events(date, current_team)
-    query_date = DateTime.parse(date) + 1.seconds
-    team_id = current_team.id
-    @events = Events.map { |class_name| class_name.constantize.team(team_id).
-      where('published_at > ?', query_date) }.flatten
+  def initialize(start_date=default_start_date, end_date=default_end_date)
+    @date_range = DateRange.new(start_date, end_date)
   end
 
-  def get_next_day(params)
-    date = params[:published_at]
-    person_ids = params.fetch(:filter, {}).fetch('person_ids', nil)
-    types      = params.fetch(:filter, {}).fetch('type', nil) || Events
-    
-    date = DateTime.parse(date).yesterday
-    start_date = date.at_beginning_of_day
-    end_date   = date.end_of_day
-    
-    if person_ids
-      search_with_person_ids(person_ids, start_date, end_date, types)
-    else
-      search_without_person_ids(start_date, end_date, types)
-    end
+  def load_recent_events(team_id)
+    unsorted_events = query_recent(team_id)
+    @events = sort_by_published_at(unsorted_events)
   end
 
-  def filter(params)
-    person_ids = params.fetch(:filter, {}).fetch('person_ids', nil)
-    types      = params.fetch(:filter, {}).fetch('type', nil) || Events
-    start_date   = params.fetch('start_date', nil) || nil
+  def find_new_events(team_id)
+    @events = query_for_new(team_id)
+  end
 
-    if start_date.present?
-      start_date = start_date.to_date.at_beginning_of_day
-      end_date   = start_date.end_of_day
-    else
-      start_date = DateTime.now - 10.days
-      end_date   = DateTime.now + 1.day
-    end
+  def query(params)
+    person_ids = params.fetch('person_ids', nil)
+    types      = params.fetch('type', nil) || Events
 
     if person_ids
-      search_with_person_ids(person_ids, start_date, end_date, types)
+      search_with_person_ids(person_ids, types)
     else
-      search_without_person_ids(start_date, end_date, types)
+      search_without_person_ids(types)
     end
-  end
-
-  #search each person passed, with or without date, for any type of event
-  def search_with_person_ids(person_ids, start_date, end_date, types)
-    self.events = person_ids.map do |person_id|
-      types.map do |type|
-        type.constantize.
-          where("person_id = ? AND published_at >= ? AND published_at <= ?", person_id, start_date, end_date)
-      end
-    end.flatten
-  end
-
-  #specific types or no types with or without date range
-  def search_without_person_ids(start_date, end_date, types)
-    self.events = types.map do |type|
-      type.constantize.where("published_at >= ? AND published_at <= ?", start_date, end_date)
-    end.flatten
   end
 
   def group_by_date
@@ -75,23 +35,54 @@ class Timeline
     format_grouped_events
   end
 
+  def sort_events_descending
+    self.events = self.events.sort_by { |event| event.published_at }.reverse
+  end
+
+  private
+
+  def search_with_person_ids(person_ids, types)
+    self.events = person_ids.map do |person_id|
+      types.map do |type|
+        type.constantize.
+          where("person_id = ? AND published_at >= ? AND published_at <= ?", person_id, date_range.start_date, date_range.end_date)
+      end
+    end.flatten
+  end
+
+  def search_without_person_ids(types)
+    self.events = types.map do |type|
+      type.constantize.where("published_at >= ? AND published_at <= ?", date_range.start_date, date_range.end_date)
+    end.flatten
+  end
+
+  def query_recent(team_id)
+    Events.map { |class_name| class_name.constantize.team(team_id).where("published_at > ?", date_range.start_date).order('published_at DESC')}.flatten
+  end
+
+  def query_for_new(team_id)
+    Events.map { |class_name| class_name.constantize.team(team_id).
+      where('published_at > ?', date_range.start_date) }.flatten
+  end
+
+  def sort_by_published_at(unsorted_events)
+    unsorted_events.sort_by { |event| event.published_at }.reverse
+  end
+
+  def format_published_at(event)
+    event.published_at.to_s(:db).split(" ").first
+  end
+
   def format_grouped_events
     formatted = self.events.map { |date, events| [date, events] }
     self.events = formatted.sort_by { |el| el[0] }.reverse
   end
 
-
-  def sort_events_descending
-    self.events = self.events.sort_by { |event| event.published_at }.reverse
+  def default_end_date
+    (Date.today + 1.day).to_s
   end
 
-
-private
-  def query_by_date(query_date, team_id)
-    Events.map { |class_name| class_name.constantize.team(team_id).where("published_at > ?", query_date ).order('published_at DESC')}.flatten
-  end
-
-  def format_published_at(event)
-    event.published_at.to_s(:db).split(" ").first 
+  def default_start_date
+    (Date.today - 10.days).to_s
   end
 end
